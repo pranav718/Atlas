47911542068cab5426d1484dea5db6c61b0d43ea
aetos <aetosdios27@gmail.com>
2025-08-12 22:38:45 +0530

    added logo and positioned it properly

--- atlas/app/page.tsx
+++ atlas/app/page.tsx
@@ -124,11 +124,11 @@ if (typeof window !== 'undefined') {
 const Header: React.FC = () => (
   <header className="relative">
     {/* Blue header strip */}
-    <div className="h-[40px] bg-[#7A96D5] flex items-center px-4">
-      <img src="/uniway.svg" alt="Uniway Logo" className="h-[30px]" />
+    <div className="h-[40px] bg-[#7A96D5] flex items-center px-4"></div>
+    {/* Cyan background with centered logo */}
+    <div className="h-[200px] bg-[#88DBE7] flex flex-col items-center justify-end relative">
+      <img src="/uniway.svg" alt="Uniway Logo" className="h-[80px] mb-20 mx-auto" />
     </div>
-    {/* Cyan background */}
-    <div className="h-[200px] bg-[#88DBE7]"></div>
     <SearchBar />
   </header>
 );
